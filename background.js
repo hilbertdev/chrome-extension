8fa50839be81936e1616bae3eedb4ff465b16da5
redpillmatrix <theredpillmatrix@gmail.com>
2020-06-13 14:45:53 +0200

    background_script_fine

--- background.js
+++ background.js
@@ -13,27 +13,14 @@ chrome.webRequest.onBeforeRequest.addListener(
     })
     return hasImage;
    }
-   function getUrls(requestObject) {
-       var imageUrl = requestObject.url;
-       console.log(imageUrl);
-       urls.push(imageUrl);
-       chrome.tabs.executeScrpit(tabId, {
-        file: "content.js"
-    }, function(){
-         chrome.tabs.sendMessage(tabId, {parameter: details.url});
-    });
-   }
     
     if (details.type === "image"){
         return {cancel: true};  
-        }
+    }
    else {
        var cancelRequest = removeImages(details.url);
-       if(cancelRequest){
-           getUrls(details.url)
-       }
     return {cancel: cancelRequest };
-       }
+    }
        },
        {types: ["sub_frame", "script", "image", "xmlhttprequest", "other"],
         urls: ["<all_urls>"]
